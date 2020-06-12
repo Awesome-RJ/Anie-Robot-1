9c2e8d97d3a64763000bc543c0410005db3feb2a
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-12 23:20:51 +0530

    Update chat_status.py

--- tg_bot/modules/helper_funcs/chat_status.py
+++ tg_bot/modules/helper_funcs/chat_status.py
@@ -316,3 +316,15 @@ def user_can_ban(func):
         return func(bot, update, *args, **kwargs)
     
     return user_is_banhammer
+
+def user_can_restrict(func):
+    @wraps(func)
+    def user_is_retrict(bot: Bot, update: Update, *args, **kwargs):
+        user = update.effective_user.id
+        member = update.effective_chat.get_member(user)
+        if not (member.can_promote_members or member.status == "creator") and not user in SUDO_USERS:
+            update.effective_message.reply_text("Sorry you don't have sufficient rights !")
+            return ""
+        return func(bot, update, *args, **kwargs)
+    
+    return user_is_restrict
